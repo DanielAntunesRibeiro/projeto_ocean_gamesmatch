a3f826923c3f7528f2a85a7fa72c9372c77c9f75
DanielAntunesRibeiro <d.a_ribeiro2@hotmail.com>
2022-10-12 18:17:50 -0400

    add classe perfil e editando base e home.html

--- gamesmatch/matchapp/models.py
+++ gamesmatch/matchapp/models.py
@@ -1,20 +1,18 @@
 from msvcrt import LK_LOCK
 from re import L
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-#class perfil(models.Model):
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+class perfil(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf = models.PositiveIntegerField.primary_key()
+    game1 = models.CharField(max_length=255)
+    pontuacao1 = models.PositiveIntegerField()
+    game2 = models.CharField(max_length=255, blank=True, null=True)
+    pontuacao2 = models.PositiveIntegerField(blank=True, null=True)
+    dat_nasc = models.DateField()
+    nickname = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True)
+    cep = models.PositiveIntegerField(blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+    zone = models.CharField(max_length=100)
